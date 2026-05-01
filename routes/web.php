e34bab7f1ec60619f82ecea6532d2b68212eb60b
Ioane Chanturia <ioane.chanturia@gmail.com>
2026-05-01 22:42:09 +0400

    Setup TaskController and routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
+
+Route::resource('tasks', TaskController::class);
+Route::patch('tasks/{task}/toggle-status', [TaskController::class, 'toggleStatus'])->name('tasks.toggle');
